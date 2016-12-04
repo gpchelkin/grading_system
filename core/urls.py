62d77c37bba0506300a2b1197ea9964942df5a64
maratkanov-a <a.s.maratkanov@gmail.com>
2016-12-04 16:29:37 +0300

    поправил урлы

--- core/urls.py
+++ core/urls.py
@@ -4,11 +4,11 @@ from core.views import TeacherListView, TeacherCreateView, TeacherUpdateView, St
     StudentCreateView
 
 urlpatterns = [
-    url(r'/teacher/(?P<pk>\d+)/update/$', TeacherUpdateView.as_view()),
-    url(r'/teacher/all/$', TeacherListView.as_view()),
-    url(r'/teacher/add/$', TeacherCreateView.as_view()),
+    url(r'teacher/(?P<pk>\d+)/update/$', TeacherUpdateView.as_view()),
+    url(r'teacher/all/$', TeacherListView.as_view()),
+    url(r'teacher/add/$', TeacherCreateView.as_view()),
 
-    url(r'/student/(?P<pk>\d+)/update/$', StudentUpdateView.as_view()),
-    url(r'/student/all/$', StudentListView.as_view()),
-    url(r'/student/add/$', StudentCreateView.as_view()),
+    url(r'student/(?P<pk>\d+)/update/$', StudentUpdateView.as_view()),
+    url(r'student/all/$', StudentListView.as_view()),
+    url(r'student/add/$', StudentCreateView.as_view()),
 ]
--- marks/urls.py
+++ marks/urls.py
@@ -5,18 +5,18 @@ from marks.views import TeacherMarkListView, TeacherMarkCreateView, TeacherMarkU
 
 urlpatterns = [
 
-    url(r'/teacher/all/$', TeacherMarkListView.as_view()),
-    url(r'/teacher/add/$', TeacherMarkCreateView.as_view()),
-    url(r'/teacher/(?P<pk>\d+)/update/$', TeacherMarkUpdateView.as_view()),
+    url(r'teacher/all/$', TeacherMarkListView.as_view()),
+    url(r'teacher/add/$', TeacherMarkCreateView.as_view()),
+    url(r'teacher/(?P<pk>\d+)/update/$', TeacherMarkUpdateView.as_view()),
 
 
 
-    url(r'/student/all/$', StudentMarkListView.as_view()),
-    url(r'/student/add/$', StudentMarkCreateView.as_view()),
-    url(r'/student/(?P<pk>\d+)/update/$', StudentMarkUpdateView.as_view()),
+    url(r'student/all/$', StudentMarkListView.as_view()),
+    url(r'student/add/$', StudentMarkCreateView.as_view()),
+    url(r'student/(?P<pk>\d+)/update/$', StudentMarkUpdateView.as_view()),
 
 
-    url(r'/subject/all/$', SubjectMarkListView.as_view()),
-    url(r'/subject/add/$', SubjectMarkCreateView.as_view()),
-    url(r'/subject/(?P<pk>\d+)/update/$', SubjectMarkUpdateView.as_view()),
+    url(r'subject/all/$', SubjectMarkListView.as_view()),
+    url(r'subject/add/$', SubjectMarkCreateView.as_view()),
+    url(r'subject/(?P<pk>\d+)/update/$', SubjectMarkUpdateView.as_view()),
 ]
