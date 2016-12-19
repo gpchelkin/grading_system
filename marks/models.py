4c4aa7ed92bdb22edb76022327a6157e14743362
maratkanov-a <a.s.maratkanov@gmail.com>
2016-12-19 14:03:32 +0300

    мелкие исправления - добавил вывод и поправил форматирование кода

--- marks/models.py
+++ marks/models.py
@@ -15,28 +15,36 @@ RATED_TYPE = (
     ('n', u'НПД')
 )
 
+
 class Scale(models.Model):
     date = models.DateTimeField(verbose_name=u'Дата заведения', default=datetime.datetime.now)
     name = models.TextField(verbose_name=u'Наименование шкалы', default='Default scale')
     max_value = models.IntegerField(verbose_name=u'Максимальное значение', default=10)
-    min_value = models.IntegerField(verbose_name=u'Минимальное значение',default=1)
+    min_value = models.IntegerField(verbose_name=u'Минимальное значение', default=1)
+
+    def __unicode__(self):
+        return u'{}'.format(self.name)
+
 
 class Krit(models.Model):
     date = models.DateTimeField(verbose_name=u'Дата заведения', default=datetime.datetime.now)
-    name = models.TextField(verbose_name=u'Наименование критерия', default = 'Default name')
+    name = models.TextField(verbose_name=u'Наименование критерия', default='Default name')
     scale = models.ForeignKey(Scale)
-    type = models.CharField(verbose_name=u'Оцениваемая сущность', default = 'p', max_length=2, choices=RATED_TYPE)
+    type = models.CharField(verbose_name=u'Оцениваемая сущность', default='p', max_length=2, choices=RATED_TYPE)
+
+    def __unicode__(self):
+        return u'Критерий {}'.format(self.name)
+
 
 class Mark(models.Model):
     date = models.DateTimeField(verbose_name=u'Дата оценивания', default=datetime.datetime.now)
     type_rated = models.CharField(verbose_name=u'Тип оценившего', default='s', max_length=2, choices=RATED_TYPE)
-    user_rated = models.ForeignKey(User, null = True,related_name='user_that_rated')
+    user_rated = models.ForeignKey(User, null=True, related_name='user_that_rated')
     type_marked = models.CharField(verbose_name=u'Тип оцениваемого', default='s', max_length=2, choices=RATED_TYPE)
     user_marked = models.ForeignKey(User, related_name='user_whom_marked', null=True, blank=True)
     subject_marked = models.ForeignKey(Subject, null=True, blank=True)
     mark_value = models.IntegerField(verbose_name=u'Оценка', default='1')
     criterion = models.ForeignKey(Krit, null=True)
 
-
-
-
+    def __unicode__(self):
+        return u'Оценка типа {}, оценил {}'.format(self.type_rated, self.type_marked)
